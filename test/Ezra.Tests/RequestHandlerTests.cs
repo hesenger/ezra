d72acecbd1ec62af32024ccc36384cc68ae8e9a9
Heber Senger <sengerhe@gmail.com>
2022-09-17 13:17:32 -0300

    cleaning up repeated tests

--- test/Ezra.Tests/RequestHandlerTests.cs
+++ test/Ezra.Tests/RequestHandlerTests.cs
@@ -15,36 +15,6 @@ public class RequestHandlerTests
         return stream;
     }
 
-    [Test]
-    public void ShouldReturn200OK()
-    {
-        var request = CreateStream("GET / HTTP/1.1\r\n");
-
-        var response = new MemoryStream();
-        var handler = new RequestHandler();
-        handler.HandleRequest(request, response);
-
-        response.Position = 0;
-        var reader = new StreamReader(response);
-        var responseText = reader.ReadToEnd();
-        Assert.AreEqual("HTTP/1.1 200 OK\r\n", responseText);
-    }
-
-    [Test]
-    public void ShouldReturn400WhenRequestIsInvalid()
-    {
-        var request = CreateStream("Invalid content");
-
-        var response = new MemoryStream();
-        var handler = new RequestHandler();
-        handler.HandleRequest(request, response);
-
-        response.Position = 0;
-        var reader = new StreamReader(response);
-        var responseText = reader.ReadToEnd();
-        Assert.AreEqual("HTTP/1.1 400 Bad Request\r\n", responseText);
-    }
-
     public static readonly string[][] GetMethodCases = new[]
     {
         new[] { "GET", "200 OK" },
