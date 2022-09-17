162d2ab8b8d8df553b7b1b3cc82cfc67f3da2a6c
Heber Senger <sengerhe@gmail.com>
2022-09-17 10:13:35 -0300

    invalid request red

--- test/Ezra.Tests/RequestHandlerTests.cs
+++ test/Ezra.Tests/RequestHandlerTests.cs
@@ -6,7 +6,7 @@ namespace Ezra.Tests;
 public class RequestHandlerTests
 {
     [Test]
-    public void ShouldReturnSuccessEmptyResponse()
+    public void ShouldReturn200OK()
     {
         var request = new MemoryStream();
         new StreamWriter(request).Write("GET / HTTP/1.1");
@@ -19,5 +19,21 @@ public class RequestHandlerTests
         var reader = new StreamReader(response);
         var responseText = reader.ReadToEnd();
         Assert.AreEqual("HTTP/1.1 200 OK\r", responseText);
+    }
+
+    [Test]
+    public void ShouldReturn400WhenRequestIsInvalid()
+    {
+        var request = new MemoryStream();
+        new StreamWriter(request).Write("Invalid content");
+
+        var response = new MemoryStream();
+        var handler = new RequestHandler();
+        handler.HandleRequest(request, response);
+
+        response.Position = 0;
+        var reader = new StreamReader(response);
+        var responseText = reader.ReadToEnd();
+        Assert.AreEqual("HTTP/1.1 400 Bad Request\r", responseText);
     }
 }
