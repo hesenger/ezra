e9735185ca6c20ad14915e6d41e67cf850e79f63
Heber Senger <sengerhe@gmail.com>
2022-09-17 10:10:29 -0300

    requesthandler green

--- src/Ezra/RequestHandler.cs
+++ src/Ezra/RequestHandler.cs
@@ -1,6 +1,16 @@
+using System.Text;
+
 namespace Ezra;
 
 public class RequestHandler
 {
-    public void HandleRequest(Stream content, Stream response) { }
+    public void HandleRequest(Stream content, Stream response)
+    {
+        using var writer = new StreamWriter(
+            stream: response,
+            encoding: Encoding.UTF8,
+            leaveOpen: true
+        );
+        writer.Write("HTTP/1.1 200 OK\r");
+    }
 }
